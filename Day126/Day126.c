e4650589252329ca2948653ff3e4c3a6e6acb15d
Aviral_Tyagi <varnikaa.1802@gmail.com>
2026-06-08 20:07:58 +0530

    Add files via upload

---
+++ Day126/Day126.c
@@ -0,0 +1,40 @@
+//Problem: Product of Array Except Self
+//Given an integer array nums, return an array answer such that:
+//answer[i] is equal to the product of all the elements of nums except nums[i].
+#include <stdio.h>
+
+void productExceptSelf(int nums[], int n, int answer[]) {
+    answer[0] = 1;
+
+    // Prefix products
+    for(int i = 1; i < n; i++) {
+        answer[i] = answer[i - 1] * nums[i - 1];
+    }
+
+    // Suffix products
+    int suffix = 1;
+    for(int i = n - 1; i >= 0; i--) {
+        answer[i] *= suffix;
+        suffix *= nums[i];
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int nums[n];
+    int answer[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    productExceptSelf(nums, n, answer);
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", answer[i]);
+    }
+
+    return 0;
+}
