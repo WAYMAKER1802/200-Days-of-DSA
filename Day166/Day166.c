1dedcbb0c704f4aa6a55fd06455bf57829c78bd9
Aviral_Tyagi <varnikaa.1802@gmail.com>
2026-07-28 22:39:10 +0530

    Add files via upload

---
+++ Day166/Day166.c
@@ -0,0 +1,40 @@
+//Problem Statement : Given the root of a binary tree, return its maximum depth.
+//The maximum depth is the number of nodes along the longest path from the root down to the farthest leaf node.
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+int main() {
+    struct TreeNode *root = createNode(3);
+    root->left = createNode(9);
+    root->right = createNode(20);
+    root->right->left = createNode(15);
+    root->right->right = createNode(7);
+
+    printf("Maximum Depth = %d\n", maxDepth(root));
+
+    return 0;
+}
